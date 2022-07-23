7c4bfe080a21be02d06cf688ae6f3ad97c3ce318
brianpaperless <brian@thepaperlessagent.com>
2022-07-23 12:58:27 -0500

    Update resume

--- scripts/resume.js
+++ scripts/resume.js
@@ -1,239 +1,224 @@
 const resume = {
   $schema:
-    "https://raw.githubusercontent.com/jsonresume/resume-schema/v1.0.0/schema.json",
+    'https://raw.githubusercontent.com/jsonresume/resume-schema/v1.0.0/schema.json',
   basics: {
-    name: "Brian Gaudino",
-    label: "Full Stack Web Developer",
-    image: "http://me.jpg",
-    email: "bgaudino@gmail.com",
-    phone: "(330) 554-6643",
-    url: "https://briangaudino.dev",
+    name: 'Brian Gaudino',
+    label: 'Full Stack Web Developer',
+    image: 'http://me.jpg',
+    email: 'bgaudino@gmail.com',
+    phone: '(330) 554-6643',
+    url: 'https://briangaudino.dev',
     summary:
       "I'm a Full Stack Web Developer based in Chicago. As a self-taught programmer with a background in music, I bring a muli-disciplinary mindset to my work. I specialize in Django and React but I'm I am always excited to learn new technologies. I'm currently working as a Software Engineer at Fusionbox.",
     location: {
-      address: "5825 S Dorchester Ave",
-      postalCode: "IL 60637",
-      city: "Chicago",
-      countryCode: "US",
+      address: '5825 S Dorchester Ave',
+      postalCode: 'IL 60637',
+      city: 'Chicago',
+      countryCode: 'US',
     },
     profiles: [
       {
-        network: "LinkedIn",
-        url: "https://linkedin.com/in/brian-gaudino",
+        network: 'LinkedIn',
+        url: 'https://linkedin.com/in/brian-gaudino',
       },
       {
-        network: "GitHub",
-        url: "https://github.com/bgaudino",
+        network: 'GitHub',
+        url: 'https://github.com/bgaudino',
       },
     ],
   },
   work: [
     {
-      name: "Fusionbox",
-      location: "Denver, CO",
-      position: "Software Engineer",
-      url: "https://fusionbox.com",
-      startDate: "2022-04-01",
-      highlights: [],
-    },
-    {
-      name: "The Paperless Agent",
-      location: "Austin, TX",
-      description: "Awesome compression company",
-      position: "Software Developer",
-      url: "https://thepaperlessagent.com",
-      startDate: "2021-07-01",
-      endDate: "2022-04-01",
+      name: 'Fusionbox',
+      location: 'Denver, CO',
+      position: 'Software Engineer',
+      url: 'https://fusionbox.com',
+      startDate: '2022-04-01',
       highlights: [
-        "Collaborate with dev team to determine the scope and expectations of the phases of our marketing suite roll out.",
-        "Produce clean, efficient code, based on team standards.",
-        "Integrate components and third-party programs through well documented APIs.",
-        "Troubleshoot, debug, and upgrade existing code as needed.",
-        "Gather and evaluate user feedback and make recommendations as necessary.",
+        'Architect and build new Python/Django applications for clients in a variety of industries',
+        'Collaborate with clients to design and implement new features',
+        'Take over and rescue existing projects',
+        'Write and maintain open-source software',
       ],
     },
     {
-      name: "Concept and Color",
-      location: "Chicago, IL",
-      description: "Awesome color company",
-      url: "https://conceptandcolor.com",
-      position: "Software Developer",
-      startDate: "2021-01-01",
-      endDate: "2021-07-01",
+      name: 'The Paperless Agent',
+      location: 'Austin, TX',
+      description: 'Awesome compression company',
+      position: 'Software Developer',
+      url: 'https://thepaperlessagent.com',
+      startDate: '2021-07-01',
+      endDate: '2022-04-01',
       highlights: [
-        "Developed and structured responsive client websites",
-        "Collaborated with a team across the stack to create feature-rich web applications.",
+        'Collaborate with dev team to determine the scope and expectations of the phases of our marketing suite roll out.',
+        'Produce clean, efficient code, based on team standards.',
+        'Integrate components and third-party programs through well documented APIs.',
+        'Troubleshoot, debug, and upgrade existing code as needed.',
+        'Gather and evaluate user feedback and make recommendations as necessary.',
       ],
     },
     {
-      name: "Cleveland Classical Guitar Society",
-      location: "Cleveland, OH",
-      position: "Director of Education",
-      startDate: "2016-08-01",
-      endDate: "2020-01-01",
+      name: 'Cleveland Classical Guitar Society',
+      location: 'Cleveland, OH',
+      position: 'Director of Education',
+      startDate: '2016-08-01',
+      endDate: '2020-04-01',
       highlights: [
-        "Engineered expansion of the Education Program from 100 students at 3 schools to over 700 students at 18 schools.",
-        "Launched Guitar@Home Academy during the COVID-19 pandemic to provide students synchronous and asynchronous online instruction opportunities.",
+        'Engineered expansion of the Education Program from 100 students at 3 schools to over 700 students at 18 schools.',
+        'Launched Guitar@Home Academy during the COVID-19 pandemic to provide students synchronous and asynchronous online instruction opportunities.',
       ],
     },
   ],
   education: [
     {
-      institution: "University of Louisiana at Lafayette",
-      studyType: "Master of Music",
-      startDate: "2007-08-01",
-      endDate: "2009-05-01",
+      institution: 'University of Louisiana at Lafayette',
+      studyType: 'Master of Music',
+      startDate: '2007-08-01',
+      endDate: '2009-05-01',
     },
     {
-      institution: "College of William and Mary",
-      studyType: "Bachelor of Arts",
-      startDate: "2003-08-01",
-      endDate: "2007-05-01",
+      institution: 'College of William and Mary',
+      studyType: 'Bachelor of Arts',
+      startDate: '2003-08-01',
+      endDate: '2007-05-01',
     },
   ],
   skills: [
     {
-      name: "Python",
+      name: 'Python',
       level: 10,
     },
     {
-      name: "Django",
+      name: 'Django',
       level: 10,
     },
     {
-      name: "React",
+      name: 'React',
       level: 9,
     },
     {
-      name: "JavaScript",
+      name: 'TypeScript',
+      level: 9,
+    },
+    ,
+    {
+      name: 'JavaScript',
       level: 9,
     },
     {
-      name: "TypeScript",
+      name: 'HTML',
       level: 8,
     },
     {
-      name: "HTML",
+      name: 'CSS',
       level: 8,
     },
     {
-      name: "CSS",
+      name: 'Git',
       level: 8,
     },
     {
-      name: "Git",
-      level: 8,
-    },
-    {
-      name: "Flask",
+      name: 'SQL',
       level: 7,
     },
     {
-      name: "Bootstrap",
+      name: 'Bootstrap',
       level: 7,
     },
     {
-      name: "Material-UI",
+      name: 'Material-UI',
       level: 7,
     },
     {
-      name: "Bulma",
+      name: 'Express',
       level: 6,
     },
     {
-      name: "Express",
+      name: 'Svelte',
       level: 5,
     },
     {
-      name: "Svelte",
+      name: 'C',
       level: 5,
     },
     {
-      name: "SQL",
-      level: 5,
-    },
-    {
-      name: "C",
-      level: 5,
-    },
-    {
-      name: "C#",
+      name: 'C#',
       level: 4,
     },
   ],
   interests: [
     {
-      name: "Music",
-      keywords: ["Guitar", "Classical", "Piano", "Violin", "Cello", "Bass"],
+      name: 'Music',
+      keywords: ['Guitar', 'Classical', 'Piano', 'Violin', 'Cello', 'Bass'],
     },
     {
-      name: "Baseball",
-      keywords: ["Baseball", "Softball", "Basketball", "Hockey"],
+      name: 'Baseball',
+      keywords: ['Baseball', 'Softball', 'Basketball', 'Hockey'],
     },
   ],
   meta: {
     canonical:
-      "https://raw.githubusercontent.com/jsonresume/resume-schema/master/resume.json",
-    version: "v1.0.0",
-    lastModified: "2017-12-24T15:53:00",
+      'https://raw.githubusercontent.com/jsonresume/resume-schema/master/resume.json',
+    version: 'v1.0.0',
+    lastModified: '2017-12-24T15:53:00',
   },
 };
 
 const monthNames = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
 ];
 
 function formatDate(date) {
-  if (!date) return "Present";
+  if (!date) return 'Present';
   const d = new Date(date);
   const year = d.getFullYear();
   const month = monthNames[(d.getMonth() + 1) % 12];
   return `${month} ${year}`;
 }
 
-const resumeContainer = document.getElementById("resume-container");
+const resumeContainer = document.getElementById('resume-container');
 
-const work = document.createElement("div");
-work.classList.add("resume-section");
-const workHeader = document.createElement("h2");
-workHeader.classList.add("spaced-header");
-workHeader.innerText = "Work";
+const work = document.createElement('div');
+work.classList.add('resume-section');
+const workHeader = document.createElement('h2');
+workHeader.classList.add('spaced-header');
+workHeader.innerText = 'Work';
 work.appendChild(workHeader);
 resumeContainer.appendChild(work);
 
-const education = document.createElement("div");
-education.classList.add("resume-section");
-const educationHeader = document.createElement("h2");
-educationHeader.innerText = "Education";
-educationHeader.classList.add("spaced-header");
+const education = document.createElement('div');
+education.classList.add('resume-section');
+const educationHeader = document.createElement('h2');
+educationHeader.innerText = 'Education';
+educationHeader.classList.add('spaced-header');
 education.appendChild(educationHeader);
 resumeContainer.appendChild(education);
 
 resume.work.forEach((job) => {
-  const div = document.createElement("div");
-  const company = document.createElement("h3");
+  const div = document.createElement('div');
+  const company = document.createElement('h3');
   company.innerText = job.name;
-  const position = document.createElement("h4");
+  const position = document.createElement('h4');
   position.innerText = job.position;
-  const time = document.createElement("h5");
+  const time = document.createElement('h5');
   time.innerText = `${formatDate(job.startDate)} - ${formatDate(job.endDate)}`;
   div.appendChild(company);
   div.appendChild(position);
   div.appendChild(time);
-  const highlights = document.createElement("ul");
+  const highlights = document.createElement('ul');
   job.highlights.forEach((highlight) => {
-    const li = document.createElement("li");
+    const li = document.createElement('li');
     li.innerText = highlight;
     highlights.appendChild(li);
   });
@@ -242,12 +227,12 @@ resume.work.forEach((job) => {
 });
 
 resume.education.forEach((school) => {
-  const div = document.createElement("div");
-  const institution = document.createElement("h3");
+  const div = document.createElement('div');
+  const institution = document.createElement('h3');
   institution.innerText = school.institution;
-  const degree = document.createElement("h4");
+  const degree = document.createElement('h4');
   degree.innerText = school.studyType;
-  const time = document.createElement("h5");
+  const time = document.createElement('h5');
   time.innerText = `${formatDate(school.startDate)} - ${formatDate(
     school.endDate
   )}`;
@@ -258,13 +243,13 @@ resume.education.forEach((school) => {
 });
 
 resume.skills.forEach((skill) => {
-  const div = document.createElement("div");
-  div.classList.add("skill");
-  const name = document.createElement("h3");
+  const div = document.createElement('div');
+  div.classList.add('skill');
+  const name = document.createElement('h3');
   name.innerText = skill.name;
   div.appendChild(name);
-  const level = document.createElement("div");
-  level.classList.add("level", "orange");
+  const level = document.createElement('div');
+  level.classList.add('level', 'orange');
   level.dataset.level = skill.level;
   div.appendChild(level);
   // for (let i = 0; i < skill.level; i++) {
@@ -272,16 +257,16 @@ resume.skills.forEach((skill) => {
   //   star.innerText = "★";
   //   div.appendChild(star);
   // }
-  document.body.querySelector("#skills").appendChild(div);
+  document.body.querySelector('#skills').appendChild(div);
 });
 
-document.addEventListener("scroll", () => {
-  const levels = document.querySelectorAll(".level");
+document.addEventListener('scroll', () => {
+  const levels = document.querySelectorAll('.level');
   levels.forEach((level) => {
     if (level.getBoundingClientRect().top <= window.innerHeight) {
       level.style.width = `${level.dataset.level * 10}%`;
     } else {
-      level.style.width = "0%";
+      level.style.width = '0%';
     }
   });
 });
